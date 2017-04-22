84a0e06e01b699a859e4763375ea13cb30068afd
Brent Thomas <brentthomas@Brents-MacBook-Air.local>
2017-04-22 16:34:19 -0600

    save before css

---
+++ public/main.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+
+$('.language1').click(function() {
+  $('.t-1').toggleClass('hidden')
+})
+
+$('.language2').click(function() {
+  $('.t-2').toggleClass('hidden')
+})
+
+$('.language3').click(function() {
+  $('.t-3').toggleClass('hidden')
+})
+
+$('.language4').click(function() {
+  $('.t-4').toggleClass('hidden')
+})
+
+$('.project-title').click(function() {
+  $('.tutorial').toggleClass('hidden')
+})
+
+
+
+})
