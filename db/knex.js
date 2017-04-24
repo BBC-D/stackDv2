74b71ea90f236cf7f3e439c80282f813a299779e
Keith Davis <kpdavis.kd@outlook.com>
2017-04-24 14:30:29 -0600

    login page loding

--- db/knex.js
+++ db/knex.js
@@ -1,6 +1,6 @@
 const config = require('../knexfile');
-const enviorment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || 'development';
 
-const pg = require('knex')(config[enviorment]);
+const pg = require('knex')(config[environment]);
 
 module.exports = pg;
