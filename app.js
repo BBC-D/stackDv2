fb4cab0f50194dbfd2298008ad071915134fc203
Keith Davis <kpdavis.kd@outlook.com>
2017-04-24 13:54:46 -0600

    app.js

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const pg = require('./db/knex');
-const app = module.export = express();
+const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('/public'));
+app.set('view engine', 'hbs');
 
+app.get('/', (req, res) => {
+  res.render('index');
+})
 
 
 app.listen(port, ()=> {
