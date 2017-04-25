f55331b1019116960c439abda5c195e5a6e8982f
Keith Davis <kpdavis.kd@outlook.com>
2017-04-25 13:57:56 -0600

    signup page creates users in db

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var pg = require('../db/knex');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/signup', function(req, res) {
   res.render('signup');
 });
-router.post('/',function(req, res) {
+router.post('/signup',function(req, res) {
   console.log(req.body);
+  var info = req.body;
+  pg('personal').insert(info).then(()=>{
+    res.redirect('/');
+  })
 } )
 
 module.exports = router;
