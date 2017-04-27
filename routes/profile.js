f06476872c167592829a3be970d9cc6dddedc913
Keith Davis <kpdavis.kd@outlook.com>
2017-04-27 11:13:19 -0600

    route url reflex user name

--- routes/profile.js
+++ routes/profile.js
@@ -1,23 +1,31 @@
 var express = require('express');
 var pg = require('../db/knex');
 var router = express.Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-/* GET profile page. */
-router.get('/:{{user.user_name}}', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* Router mounted at "url/profile/" */
+router.get('/:user_name', (req, res) =>{
+  var user = req.params.user_name
+  pg('personal').select().where('user_name', user).then((info) => {
+    res.render('profile', {user: info[0]});
+  })
 
+})
 router.post('/', (req, res) =>{
   var user = req.body;
-
   pg('personal').select().where('user_name', user.user_name).then((info) => {
-
-    if (info[0].password === user.password) {
-    res.redirect('/profile', {user: info[0]})
-    }else{
-      res.redirect('/');
-    }
-  })
-})
+    console.log(info[0].id);
+    bcrypt.compare(user.password, info[0].password).then((pass) => {
+      if (pass) {
+        // pg('personal').join('project').where('personal.id','project.owner').then((user) => {
+          res.redirect('/profile/' + user.user_name);
+        // })
+      }else{
+          res.redirect('/');
+      }
+    })
+  });
+});
 
 module.exports = router;
